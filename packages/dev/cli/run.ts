e5e99d9b0b880d1cff0a5352e6ee632d047ae1a0
Abdur-Rahman <abbadshaf05@gmail.com>
2023-08-07 16:54:10 +0100

    feat: `cli` coming together

--- packages/dev/cli/run.ts
+++ packages/dev/cli/run.ts
@@ -1,6 +1,9 @@
-import { bold, magenta, underline, whiteBright } from "colorette";
+import { bold, magenta, red, underline, whiteBright } from "colorette";
 import arg from "arg";
 import inquirer from "inquirer";
+import { pathExists } from "fs-extra";
+import { resolve } from "path";
+import { detectPackageManager } from "./detectPkgManager.js";
 
 // Todo(ShafSpecs): Update this later
 const helpText = `
@@ -38,7 +41,12 @@ ${underline(whiteBright("Options:"))}
 --version, -v                   Print the CLI version and exit
 --docs                          Print the link to the remix-pwa docs and exit`;
 
-export async function run(argv: string[] = process.argv.slice(2)) {
+type PWAFeatures = "sw" | "manifest" | "push" | "utils" | "icons";
+
+export async function run(
+  argv: string[] = process.argv.slice(2),
+  projectDir: string = process.cwd()
+) {
   // todo: Allow passing configs via CLI like minify build, worker path, etc.
   const args = arg(
     {
@@ -52,7 +60,7 @@ export async function run(argv: string[] = process.argv.slice(2)) {
       "--install": Boolean,
       "--no-install": Boolean,
       "--docs": Boolean,
-      "--cache": String,
+      "--cache": Boolean,
       "--features": String,
       "--dir": String,
       "--package-manager": String,
@@ -74,7 +82,12 @@ export async function run(argv: string[] = process.argv.slice(2)) {
 
   if (args["--help"]) {
     console.log(helpText);
-    process.exit(0);
+    return;
+  }
+
+  if (args["--docs"]) {
+    console.log("https://remix-pwa-docs.vercel.app");
+    return;
   }
 
   // Handle other flags here later
@@ -82,16 +95,147 @@ export async function run(argv: string[] = process.argv.slice(2)) {
   const cmd = input[0];
 
   switch (cmd) {
-    case "init":
-    case "new":
-    case "create":
-      break;
     case "dev":
       break;
     case "build":
       break;
-    default:
-      console.log(helpText); // Todo: switch this to `dev` later - remix-pwa dev == remix-pwa 
-      process.exit(0);
+    case "init":
+    case "new":
+    case "create":
+    default: // Todo: Add a better error message - Deprecating this. For now tho, it would be the same as create
+      if (cmd !== "create" && cmd !== "init" && cmd !== "new") {
+        console.warn(
+          red(
+            "This command is getting deprecated soon. Please use `create` instead."
+          )
+        );
+      }
+
+      let packageManager = (await detectPackageManager(projectDir)) ?? "npm";
+
+      const answers = await inquirer
+        .prompt<{
+          lang: "Typescript" | "JavaScript";
+          workbox: boolean;
+          install: boolean;
+          precache: boolean;
+          features: PWAFeatures[];
+          dir: string;
+          packageManager: string;
+        }>([
+          {
+            type: "list",
+            name: "lang",
+            message:
+              "Is this a TypeScript or JavaScript project? Pick the opposite for chaos!",
+            when: !args["--typescript"] && !args["--no-typescript"],
+            choices: ["Typescript", "JavaScript"],
+          },
+          {
+            type: "confirm",
+            name: "workbox",
+            message: "Do you want to integrate workbox into your project?",
+            default: false,
+            when: !args["--workbox"] && !args["--no-workbox"],
+          },
+          {
+            type: "confirm",
+            name: "precache",
+            message: "Do you want to utilise precaching in this project?",
+            default: false,
+            when: !args["--cache"],
+          },
+          {
+            type: "checkbox",
+            name: "features",
+            message: "What features do you want to include?",
+            choices: [
+              {
+                name: "Service Workers",
+                value: "sw",
+              },
+              {
+                name: "Web Manifest",
+                value: "manifest",
+              },
+              {
+                name: "Push Notifications",
+                value: "push",
+              },
+              {
+                name: "PWA Client Utilities",
+                value: "utils",
+              },
+              {
+                name: "Development Icons",
+                value: "icons",
+              },
+            ],
+            default: ["sw", "manifest"],
+            when: !args["--features"],
+          },
+          {
+            type: "input",
+            name: "dir",
+            message: "Where is your Remix `app` directory located?",
+            default: "app",
+            when: !args["--dir"],
+            validate(input: string, answers) {
+              if (input === "") {
+                return "Please enter a valid directory";
+              }
+
+              pathExists(resolve(projectDir, input)).then((exists) => {
+                if (!exists) {
+                  return "Please enter a valid directory";
+                }
+              });
+
+              if (input.startsWith("/") || input.endsWith("/")) {
+                answers!.dir = input.replace(/^\/|\/$/g, "");
+              }
+
+              return true;
+            },
+          },
+          {
+            type: "list",
+            name: "packageManager",
+            message: "What package manager do you use?",
+            choices: ["npm", "yarn", "pnpm"],
+            default: "npm",
+            when:
+              !args["--package-manager"] &&
+              (await detectPackageManager(projectDir)) === undefined,
+          },
+          {
+            type: "confirm",
+            name: "install",
+            message: `Do you want to run ${packageManager} install?`,
+            default: true,
+            when: !args["--install"] && !args["--no-install"],
+          },
+        ])
+        .catch((err) => {
+          if (err.isTtyError) {
+            console.error(
+              red(
+                "💥 Your terminal doesn't support interactivity! Prompt couldn't be rendered in the current environment"
+              )
+            );
+
+            return {
+              lang: "Typescript",
+              workbox: false,
+              install: true,
+              precache: false,
+              features: ["sw", "manifest"],
+              dir: "app",
+              packageManager,
+            };
+          }
+
+          throw err;
+        });
   }
 }
