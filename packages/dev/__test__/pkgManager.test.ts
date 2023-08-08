9a428b65c2eb781702607308a4337f00036ccd05
Abdur-Rahman <abbadshaf05@gmail.com>
2023-08-08 21:50:42 +0100

    test: updated test for global configs :+1:

--- packages/dev/__test__/pkgManager.test.ts
+++ packages/dev/__test__/pkgManager.test.ts
@@ -1,16 +1,14 @@
-import { resolve } from 'path';
-
 import { detectPackageManager } from '../cli/detectPkgManager.ts';
 
 describe('Package Manager Test Suite', () => {
   it('should return undefined when no lock file is found', async () => {
-    const pkgManager = await detectPackageManager(process.cwd());
+    const pkgManager = await detectPackageManager('./packages');
 
     expect(pkgManager).toBe(undefined);
   });
 
   it('should return npm when package-lock.json is found', async () => {
-    const pkgManager = await detectPackageManager(resolve(process.cwd(), '../../'));
+    const pkgManager = await detectPackageManager(process.cwd());
 
     expect(pkgManager).toBe('npm');
   });
