2b7f18142a0af5d8b4a49fc7c65ba2e457a684e4
Abdur-Rahman <abbadshaf05@gmail.com>
2023-08-08 17:57:42 +0100

    refactor: renamed `*.spec.ts` to `*.test.ts`

---
+++ packages/dev/__test__/pkgManager.test.ts
@@ -0,0 +1,5 @@
+describe('Package Manager Test Suite', () => {
+  it('should be true', () => {
+    expect(true).toBe(true);
+  });
+});
--- packages/dev/cli/__test__/pkgManager.spec.ts
+++
@@ -1,7 +0,0 @@
-// Requires a jest config file with moduleNameMapper
-
-test('Package Manager', () => {
-  it('should detect package manager', () => {
-    expect(true).toBe(true);
-  });
-});
