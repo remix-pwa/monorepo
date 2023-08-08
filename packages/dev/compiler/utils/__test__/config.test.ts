19b847dd8ec4376664cfc1656864d16908f84fa0
Luciano Fantone <luciano.alvarezfantone@sas.se>
2023-08-08 10:36:15 +0200

    test(dev-compiler): mock only for custom config values
    Co-authored-by: Juan Pablo Garcia Ripa <juanpablo.garciaripa@sas.se>

--- packages/dev/compiler/utils/__test__/config.test.ts
+++ packages/dev/compiler/utils/__test__/config.test.ts
@@ -1,74 +1,74 @@
-import { jest } from '@jest/globals';
-import { findConfig, readConfig as _readConfig } from '@remix-run/dev/dist/config.js';
-import type { ResolvedRemixConfig } from '@remix-run/dev';
-import { ServerMode } from '@remix-run/dev/dist/config/serverModes.js';
-import readConfig from '../config.js';
+import { jest } from "@jest/globals";
+import type { ResolvedRemixConfig } from "@remix-run/dev";
+import {
+  readConfig as _readConfig,
+  findConfig,
+} from "@remix-run/dev/dist/config.js";
+import { ServerMode } from "@remix-run/dev/dist/config/serverModes.js";
+import readConfig from "../config.ts";
 
-const REMIX_ROOT = '.';
-var mockWorkerConfigModule = jest.fn();
+const REMIX_ROOT = ".";
 
-jest.mock('@remix-run/dev/dist/config');
-jest.mock(`./remix.config.ts`, () => ({
-  default: mockWorkerConfigModule(),
-}));
-
+jest.mock("@remix-run/dev/dist/config.js");
 jest.mocked(_readConfig).mockResolvedValue({
-  appDirectory: 'app',
-  assetsBuildDirectory: 'public/build',
-  ignoredRouteFiles: ['**/.*'],
-  serverPlatform: 'node',
-  serverModuleFormat: 'cjs',
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  ignoredRouteFiles: ["**/.*"],
+  serverPlatform: "node",
+  serverModuleFormat: "cjs",
 } as unknown as ResolvedRemixConfig);
+jest.mocked(findConfig).mockReturnValue("./__test__/remix.config.ts");
 
-jest.mocked(findConfig).mockReturnValue('./__test__/remix.config.ts');
-
-describe('readConfig', () => {
+describe("readConfig", () => {
   afterAll(() => {
-    jest.unmock('@remix-run/dev/dist/config');
+    jest.unmock("@remix-run/dev/dist/config");
     jest.unmock(`./remix.config.ts`);
   });
 
-  it('should return the resolved config object with default worker options', async () => {
-    mockWorkerConfigModule.mockReturnValue({});
+  it("should return the resolved config object with default worker options", async () => {
     const config = await readConfig(REMIX_ROOT, ServerMode.Test);
 
     expect(config).toEqual({
-      appDirectory: 'app',
-      assetsBuildDirectory: 'public/build',
-      entryWorkerFile: expect.stringContaining('entry.worker.js'),
-      ignoredRouteFiles: ['**/.*'],
-      serverModuleFormat: 'cjs',
-      serverPlatform: 'node',
-      worker: expect.stringContaining('service-worker.internal.js'),
-      workerBuildDirectory: expect.stringContaining('public'),
+      appDirectory: "app",
+      assetsBuildDirectory: "public/build",
+      entryWorkerFile: expect.stringContaining("entry.worker.js"),
+      ignoredRouteFiles: ["**/.*"],
+      serverModuleFormat: "cjs",
+      serverPlatform: "node",
+      worker: expect.stringContaining("service-worker.internal.js"),
+      workerBuildDirectory: expect.stringContaining("public"),
       workerMinify: false,
-      workerName: 'service-worker',
+      workerName: "service-worker",
       workerSourcemap: false,
     });
   });
 
-  it('should return the resolved config object with custom worker options', async () => {
-    mockWorkerConfigModule.mockReturnValue({
-      worker: 'custom-service-worker.js',
-      workerMinify: true,
-      workerName: 'sw.js',
-      workerSourcemap: true,
+  it("should return the resolved config object with custom worker options", async () => {
+    jest.mock(`./remix.config.ts`, () => {
+      return {
+        default: {
+          worker: "custom-service-worker.js",
+          workerMinify: true,
+          workerName: "sw",
+          workerSourcemap: true,
+        },
+      };
     });
-    const { default: readConfig } = await import('../config.js');
+    const { default: readConfig } = await import("../config.ts");
     const config = await readConfig(REMIX_ROOT, ServerMode.Test);
 
     expect(config).toEqual({
-      appDirectory: 'app',
-      assetsBuildDirectory: 'public/build',
-      entryWorkerFile: expect.stringContaining('entry.worker.js'),
-      ignoredRouteFiles: ['**/.*'],
-      serverModuleFormat: 'cjs',
-      serverPlatform: 'node',
-      worker: expect.stringContaining('service-worker.internal.js'),
-      workerBuildDirectory: expect.stringContaining('public'),
-      workerMinify: false,
-      workerName: 'service-worker',
-      workerSourcemap: false,
+      appDirectory: "app",
+      assetsBuildDirectory: "public/build",
+      entryWorkerFile: expect.stringContaining("entry.worker.js"),
+      ignoredRouteFiles: ["**/.*"],
+      serverModuleFormat: "cjs",
+      serverPlatform: "node",
+      worker: "custom-service-worker.js",
+      workerBuildDirectory: expect.stringContaining("public"),
+      workerMinify: true,
+      workerName: "sw",
+      workerSourcemap: true,
     });
   });
 });
--- packages/dev/compiler/utils/config.ts
+++ packages/dev/compiler/utils/config.ts
@@ -1,49 +1,59 @@
-import { findConfig, readConfig as _readConfig } from '@remix-run/dev/dist/config.js';
-import { ServerMode } from '@remix-run/dev/dist/config/serverModes.js';
-import type { AppConfig, ResolvedRemixConfig } from '@remix-run/dev';
-import { resolve } from 'node:path';
+import type { AppConfig, ResolvedRemixConfig } from "@remix-run/dev";
+import {
+  readConfig as _readConfig,
+  findConfig,
+} from "@remix-run/dev/dist/config.js";
+import { ServerMode } from "@remix-run/dev/dist/config/serverModes.js";
+import { resolve } from "node:path";
 
-const EXTENSIONS = ['.js', '.mjs', '.cjs'];
+const EXTENSIONS = [".js", ".mjs", ".cjs"];
 
 interface Config {
   /**
    * The path to the user custom worker implementation file.
    */
-  worker?: string,
+  worker?: string;
   /**
- * The file name of the final bundled worker file.
- */
-  workerName?: string,
+   * The file name of the final bundled worker file.
+   */
+  workerName?: string;
   /**
-  * Whether to minify the worker file.
-  */
-  workerMinify?: boolean,
+   * Whether to minify the worker file.
+   */
+  workerMinify?: boolean;
   /**
-  * The directory to write the worker file to.
-  */
-  workerBuildDirectory?: string,
+   * The directory to write the worker file to.
+   */
+  workerBuildDirectory?: string;
   /**
-  * Whether to generate a sourcemap for the worker file.
-  */
-  workerSourcemap?: boolean
-};
+   * Whether to generate a sourcemap for the worker file.
+   */
+  workerSourcemap?: boolean;
+}
 
 export type WorkerConfig = AppConfig & Config;
-export type ResolvedWorkerConfig = ResolvedRemixConfig & Required<Config> & { entryWorkerFile: string };
+export type ResolvedWorkerConfig = ResolvedRemixConfig &
+  Required<Config> & { entryWorkerFile: string };
 
 /**
  * Reads the remix.config.js file and returns the config object.
  */
-export default async function readConfig(remixRoot: string, mode: ServerMode): Promise<ResolvedWorkerConfig> {
+export default async function readConfig(
+  remixRoot: string,
+  mode: ServerMode
+): Promise<ResolvedWorkerConfig> {
   const remixConfig = await _readConfig(remixRoot, mode);
-  const workerConfig = await import(findConfig(remixRoot, 'remix.config', EXTENSIONS) as string).then(m => m.default ?? m);
+  const workerConfig = await import(
+    findConfig(remixRoot, "remix.config", EXTENSIONS) as string
+  ).then((m) => m.default ?? m);
 
   return {
     ...remixConfig,
-    entryWorkerFile: resolve(remixConfig.appDirectory, 'entry.worker.js'),
-    worker: workerConfig.worker ?? require.resolve('@remix-pwa/worker-runtime'),
-    workerBuildDirectory: workerConfig.workerBuildDirectory ?? resolve('./public'),
-    workerName: workerConfig.workerName ?? 'service-worker',
+    entryWorkerFile: resolve(remixConfig.appDirectory, "entry.worker.js"),
+    worker: workerConfig.worker ?? require.resolve("@remix-pwa/worker-runtime"),
+    workerBuildDirectory:
+      workerConfig.workerBuildDirectory ?? resolve("./public"),
+    workerName: workerConfig.workerName ?? "service-worker",
     workerMinify: workerConfig.workerMinify ?? false,
     workerSourcemap: workerConfig.workerSourcemap ?? false,
   };
