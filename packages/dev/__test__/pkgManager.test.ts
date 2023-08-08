dced3e10f470da5b7489645a69c3491b3f8f2a81
Abdur-Rahman <abbadshaf05@gmail.com>
2023-08-08 18:40:37 +0100

    test: packageManager test

--- packages/dev/__test__/pkgManager.test.ts
+++ packages/dev/__test__/pkgManager.test.ts
@@ -1,5 +1,17 @@
+import { resolve } from 'path';
+
+import { detectPackageManager } from '../cli/detectPkgManager.ts';
+
 describe('Package Manager Test Suite', () => {
-  it('should be true', () => {
-    expect(true).toBe(true);
+  it('should return undefined when no lock file is found', async () => {
+    const pkgManager = await detectPackageManager(process.cwd());
+
+    expect(pkgManager).toBe(undefined);
+  });
+
+  it('should return npm when package-lock.json is found', async () => {
+    const pkgManager = await detectPackageManager(resolve(process.cwd(), '../../'));
+
+    expect(pkgManager).toBe('npm');
   });
 });
