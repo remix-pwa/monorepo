fa6a8358ece95920ad52f4347b4c1a4acd1c716e
Abdur-Rahman <abbadshaf05@gmail.com>
2023-08-09 22:08:59 +0100

    refactor

--- packages/dev/cli/commands.ts
+++ packages/dev/cli/commands.ts
@@ -1,11 +1,11 @@
-import { createPWA } from './create.js';
+import { FlagOptionType, createPWA } from './create.js';
 
 export const dev = async (argv: string[] = process.argv.slice(2), projectDir: string = process.cwd()) => {};
 
 export const build = async (argv: string[] = process.argv.slice(2), projectDir: string = process.cwd()) => {};
 
 export const push = async () => {};
-// todo: typings
-export const init = async (answer: any) => {
-  await createPWA();
+
+export const init = async (dir: string, answer: FlagOptionType) => {
+  await createPWA(dir, answer);
 };
