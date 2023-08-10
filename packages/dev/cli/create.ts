78695238767ec2a03e096cc30b8ca4dd8eb27516
Abdur-Rahman <abbadshaf05@gmail.com>
2023-08-10 01:24:41 +0100

    feat: manifest integration

--- packages/dev/cli/create.ts
+++ packages/dev/cli/create.ts
@@ -1,105 +1,152 @@
-import { resolve } from "path";
-import { PWAFeatures } from "./run.js";
-import { pathExists, pathExistsSync, readFileSync, writeFile } from "fs-extra";
-import { red } from "colorette";
+import { red } from 'colorette';
+import { pathExistsSync, writeFile, readFileSync } from 'fs-extra';
+import { resolve } from 'path';
+import { PWAFeatures } from './run.js';
+import { readFile } from 'fs';
+
+let isV2 = false;
 
 export type FlagOptionType = {
   dir: string;
   precache: boolean;
   install: boolean;
   workbox: boolean;
-  lang: "ts" | "js";
+  lang: 'ts' | 'js';
   features: PWAFeatures[];
   packageManager: string;
 };
 
-async function integrateServiceWorker(projectDir: string, precache: boolean, workbox: boolean, lang: "ts" | "js" = "ts") {
-  const templateDir = resolve(__dirname, "../../../templates");
+async function integrateServiceWorker(
+  projectDir: string,
+  precache: boolean,
+  _workbox: boolean,
+  lang: 'ts' | 'js' = 'ts',
+  dir: string = 'app'
+) {
+  const templateDir = resolve(__dirname, 'templates');
 
-  if (!pathExistsSync(templateDir)) {
-    throw new Error("Template directory not found");
-  }
-
-  console.log("Integrating service worker..."); // todo: ora spinners 
-  console.log(projectDir)
+  console.log('Integrating Service Worker...'); // todo: ora spinners for each step
 
   if (precache) {
     // if (workbox) { return; }
-    const workerDir = resolve(projectDir, `entry.worker.${lang}`)
+    const workerDir = resolve(projectDir, dir, `entry.worker.${lang}`);
 
     if (pathExistsSync(workerDir)) {
-      console.log(red("Service worker already exists"));
+      console.log(red('Service worker already exists'));
     } else {
-      const workerContent = readFileSync(resolve(templateDir, "app", `precache.worker.${lang}`), "utf-8");
+      const workerContent = readFileSync(resolve(templateDir, 'app', `precache.worker.${lang}`), 'utf-8');
 
-      await writeFile(workerDir, workerContent, "utf-8")
+      await writeFile(workerDir, workerContent, 'utf-8');
     }
   } else {
     // if (workbox) { return; }
 
-    const workerDir = resolve(projectDir, `entry.worker.${lang}`)
+    const workerDir = resolve(projectDir, dir, `entry.worker.${lang}`);
 
     if (pathExistsSync(workerDir)) {
-      console.log(red("Service worker already exists"));
+      console.log(red('Service worker already exists'));
     } else {
-      const workerContent = readFileSync(resolve(templateDir, "app", `entry.worker.${lang}`), "utf-8");
+      const workerContent = readFileSync(resolve(templateDir, 'app', `entry.worker.${lang}`), 'utf-8');
 
-      await writeFile(workerDir, workerContent, "utf-8")
+      await writeFile(workerDir, workerContent, 'utf-8');
     }
   }
 }
 
+async function integrateManifest(
+  projectDir: string,
+  lang: 'ts' | 'js' = 'ts',
+  dir: string = 'app'
+  ){
+    const templateDir = resolve(__dirname, 'templates');
+    const manifestDir = resolve(projectDir, dir, `routes/manifest[.]webmanifest.${lang}`);
+
+    console.log('Integrating Web Manifest...'); // todo: ora spinners for each step
+
+    if (pathExistsSync(manifestDir)) {
+      return;
+    } 
+
+    const manifestContent = readFileSync(resolve(templateDir, 'app', `manifest[.]webmanifest.js`), 'utf-8');
+    await writeFile(manifestDir, manifestContent, 'utf-8');
+  }
+
 export async function createPWA(
-  projectDir: string = process.cwd(), 
+  projectDir: string = process.cwd(),
   options: FlagOptionType = {
-  dir: projectDir,
-  precache: false,
-  install: true,
-  workbox: false,
-  lang: "ts",
-  features: ['sw', 'manifest'],
-  packageManager: "npm"
-}) {
+    dir: 'app',
+    precache: false,
+    install: true,
+    workbox: false,
+    lang: 'ts',
+    features: ['sw', 'manifest'],
+    packageManager: 'npm',
+  }
+) {
   let { dir, precache, install, workbox, lang, features, packageManager } = options;
 
   if (workbox) {
     workbox = false;
   }
 
-  const templateDir = resolve(__dirname, "../../../templates");
+  readFile(resolve(projectDir, 'remix.config.js'), 'utf-8', (err, data) => {
+    if (err) {
+      console.log(
+        red(
+          "No `remix.config.js` file found in your project. Please make sure to run in a remix project or create one and try again or alternatively, run `remix-pwa --help` for more info.",
+        ),
+      );
+      return;
+    }
+
+    const remixConfig = eval(data);
+    if (!remixConfig.future) {
+      remixConfig.future = {};
+    }
+
+    if (remixConfig.future && remixConfig.future.v2_routeConvention == true) {
+      isV2 = true;
+    }
+  });
+
+  const templateDir = resolve(__dirname, 'templates');
 
   if (!pathExistsSync(templateDir)) {
-    throw new Error("Template directory not found");
+    throw new Error('Template directory not found');
   }
 
-  features.map(async (feature) => {
+  features.map(async feature => {
     switch (feature) {
-      case "sw":
+      case 'sw':
         try {
-          await integrateServiceWorker(dir, precache, workbox, lang);
+          await integrateServiceWorker(projectDir, precache, workbox, lang, dir);
         } catch (err) {
-          console.log(typeof err == "string" ? red(err) : err);
+          console.log(typeof err == 'string' ? red(err) : err);
         }
         break;
-      case "manifest":
-        console.log("manifest");
+      case 'manifest':
+        try {
+          await integrateManifest(projectDir, lang, dir);
+        } catch (err) {
+          console.log(typeof err == 'string' ? red(err) : err);
+        }
         break;
-      case "icons":
-        console.log("workbox");
+      case 'icons':
+        console.log('workbox');
         break;
-      case "push":
-        console.log("install");
+      case 'push':
+        console.log('install');
         break;
-      case "utils":
-        console.log("precache");
+      case 'utils':
+        console.log('precache');
         break;
       default:
         break;
     }
-  })
+  });
 
   if (install) {
-    console.log("installing deps with", packageManager);
+    console.log('installing deps with', packageManager);
   } else {
     console.log(red(`Skipping ${packageManager} install...\n`));
     console.log(red(`Don't forget to run ${packageManager} install`));
