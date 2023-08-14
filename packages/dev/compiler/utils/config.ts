2f932b37f719db50c4446ce0cefda1abbfb84ec0
Luciano Fantone <luciano.alvarezfantone@sas.se>
2023-08-14 12:55:27 +0200

    feat(compiler): allow any javascript related extension as user entry
    Co-authored-by: Juan Pablo Garcia Ripa <juanpablo.garciaripa@sas.se>

--- packages/dev/compiler/utils/config.ts
+++ packages/dev/compiler/utils/config.ts
@@ -1,48 +1,50 @@
 import type { AppConfig, ResolvedRemixConfig } from '@remix-run/dev';
 import { readConfig as _readConfig, findConfig } from '@remix-run/dev/dist/config.js';
 import type { ServerMode } from '@remix-run/dev/dist/config/serverModes.js';
+import { existsSync } from 'node:fs';
 import { createRequire } from 'node:module';
-import { resolve } from 'node:path';
+import { relative, resolve } from 'node:path';
 
 const _require = createRequire(import.meta.url);
 
-const EXTENSIONS = ['.js', '.mjs', '.cjs'];
+const EXTENSIONS = ['.js', '.mjs', '.cjs', '.ts'];
+const DEFAULT_ENTRY_WORKER_FILE = 'entry.worker.ts';
 
 interface Config {
   /**
    * The path to the entry worker file.
-   * 
+   *
    * @default '<appDir>/entry.worker.ts'
    */
-  entryWorkerFile?: string; 
+  entryWorkerFile?: string;
   /**
    * The path to the user custom worker implementation runtime.
-   * 
+   *
    * @default '@remix-pwa/worker-runtime'
    */
   worker?: string;
   /**
-   * The file name of the final bundled worker file. 
+   * The file name of the final bundled worker file.
    * **Without the extension.**
-   * 
+   *
    * @default 'entry.worker'
    */
   workerName?: string;
   /**
    * Whether to minify the worker file.
-   * 
+   *
    * @default false
    */
   workerMinify?: boolean;
   /**
    * The directory to write the worker file to.
-   * 
+   *
    * @default '<root>/public'
    */
   workerBuildDirectory?: string;
   /**
    * Whether to generate a sourcemap for the worker file.
-   * 
+   *
    * @default false
    */
   workerSourcemap?: boolean;
@@ -51,18 +53,28 @@ interface Config {
 export type WorkerConfig = AppConfig & Config;
 export type ResolvedWorkerConfig = ResolvedRemixConfig & Required<Config>;
 
+function findEntry(dirname: string, basename: string): string | undefined {
+  for (const extension of EXTENSIONS) {
+    const file = resolve(dirname, `${basename}${extension}`);
+    if (existsSync(file)) {
+      return relative(dirname, file);
+    }
+  }
+}
+
 /**
  * Reads the remix.config.js file and returns the config object.
  */
 export default async function readConfig(remixRoot: string, mode: ServerMode): Promise<ResolvedWorkerConfig> {
   const remixConfig = await _readConfig(remixRoot, mode);
-  const workerConfig = await import(findConfig(remixRoot, 'remix.config', EXTENSIONS) as string).then(
+  const workerConfig = (await import(findConfig(remixRoot, 'remix.config', EXTENSIONS) as string).then(
     m => m.default ?? m
-  ) as WorkerConfig;
+  )) as WorkerConfig;
 
   return {
     ...remixConfig,
-    entryWorkerFile: workerConfig.entryWorkerFile ?? resolve(remixConfig.appDirectory, 'entry.worker.ts'),
+    entryWorkerFile:
+      workerConfig.entryWorkerFile ?? findEntry(remixConfig.appDirectory, 'entry.worker') ?? DEFAULT_ENTRY_WORKER_FILE,
     worker: workerConfig.worker ?? _require.resolve('@remix-pwa/worker-runtime'),
     workerBuildDirectory: workerConfig.workerBuildDirectory ?? resolve('./public'),
     workerName: workerConfig.workerName ?? 'entry.worker',
