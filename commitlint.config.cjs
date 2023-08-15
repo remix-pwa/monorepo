16034b4217a4c958fe8247972ea1430b5ab2174c
Abdur-Rahman <abbadshaf05@gmail.com>
2023-08-15 09:39:40 +0100

    refactor(ci): added comments regarding types

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,16 @@
 module.exports = { extends: ['@commitlint/config-conventional'] }
+/**
+ * Accepted types:
+ * 
+ * build
+ * chore
+ * ci
+ * docs
+ * feat
+ * fix
+ * perf
+ * refactor
+ * revert
+ * style
+ * test
+ */
