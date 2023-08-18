519d373fc92382f23e527fc744c0927b4b7e85fb
Abdur-Rahman <abbadshaf05@gmail.com>
2023-08-18 14:38:20 +0100

    feat(client): exported all new API functions

--- packages/client/index.ts
+++ packages/client/index.ts
@@ -1,1 +1,5 @@
-export { copyTextToClipboard, pasteFromClipboard } from './lib/clipboard';
+export { clearBadge, setBadge } from './lib/badge';
+export { copyImageToClipboard, copyTextToClipboard, readFiles, readTextFromClipboard } from './lib/clipboard';
+export { checkConnectivity } from './lib/connectivity';
+export { shareData } from './lib/share';
+export { checkVisibility, wakeLock } from './lib/usage';
