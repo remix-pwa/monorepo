f9237568cc7b68273cac568075faa073c56d8174
Abdur-Rahman <abbadshaf05@gmail.com>
2023-08-19 17:21:15 +0100

    chore(cache): exposed more APIs to work with `@remix-pwa/cache` This contains APIs like more `cachified` wrappers and type exports

--- packages/cache/index.ts
+++ packages/cache/index.ts
@@ -1,2 +1,4 @@
 export { remixCacheAdapter as remixCachifiedAdapter } from './src/adapter.js';
+export { RemixCacheOptions, Strategy } from './src/cache.js';
+export { CachifiedWrapperOptions, cachifiedWrapper } from './src/cachified.js';
 export { RemixCacheStorage, Storage, createCache, initCache } from './src/storage.js';
