8d22aa2493e5ba0797f0e7b987c70b747434e3f6
Abdur-Rahman <abbadshaf05@gmail.com>
2023-08-29 10:41:57 +0100

    chore(cache): exported `RemixCache` as a type

--- packages/cache/index.ts
+++ packages/cache/index.ts
@@ -2,3 +2,5 @@ export { remixCacheAdapter as remixCachifiedAdapter } from './src/adapter.js';
 export { RemixCacheOptions, Strategy } from './src/cache.js';
 export { CachifiedWrapperOptions, cachifiedWrapper } from './src/cachified.js';
 export { RemixCacheStorage, Storage, createCache, initCache } from './src/storage.js';
+
+export type { RemixCache } from './src/cache.js';
