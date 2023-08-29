17e7b1d7121319d73d98bcaab88009665ccda72a
Abdur-Rahman <abbadshaf05@gmail.com>
2023-08-29 10:45:44 +0100

    chore(strategy): package utils -- private

---
+++ packages/strategy/src/utils.ts
@@ -0,0 +1,11 @@
+/**
+ * Returns a promise that resolves and the passed number of milliseconds.
+ * This utility is an async/await-friendly version of `setTimeout`.
+ *
+ * @param {number} ms
+ * @return {Promise}
+ * @private
+ */
+export function timeout(ms: number): Promise<unknown> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
