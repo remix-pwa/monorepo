6ce80e7acaf61c37f751b9054906548a8626d2a5
Abdur-Rahman <abbadshaf05@gmail.com>
2023-08-30 10:41:09 +0100

    chore(strategy): exported utils

--- packages/strategy/index.ts
+++ packages/strategy/index.ts
@@ -3,3 +3,5 @@ export { CacheOnlyStrategyOptions, cacheOnly } from './src/cacheOnly.js';
 export { NetworkFirstStrategyOptions, networkFirst } from './src/networkFirst.js';
 export { NetworkOnlyStrategyOptions, networkOnly } from './src/networkOnly.js';
 export { StaleWhileRevalidateStrategyOptions, staleWhileRevalidate, swr } from './src/staleWhileRevalidate.js';
+
+export { toJSON } from './src/utils.js';
