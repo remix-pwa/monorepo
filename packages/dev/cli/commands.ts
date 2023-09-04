a158caab1971f6427269653a6859aa829c629de7
Abdur-Rahman <abbadshaf05@gmail.com>
2023-09-04 08:36:14 +0100

    feat(dev): added `packages` command to CLI

--- packages/dev/cli/commands.ts
+++ packages/dev/cli/commands.ts
@@ -1,8 +1,10 @@
-import { yellow } from 'colorette';
+import { execSync } from 'child_process';
+import { blue, yellow } from 'colorette';
 
 import { runCompiler } from '../compiler/compiler.js';
 import type { FlagOptionType } from './create.js';
 import { createPWA } from './create.js';
+import { detectPackageManager } from './detectPkgManager.js';
 
 export const dev = async (projectDir: string = process.cwd()) => {
   await runCompiler('dev', projectDir);
@@ -18,4 +20,25 @@ export const push = async () => {
 
 export const init = async (dir: string, answer: FlagOptionType) => {
   await createPWA(dir, answer);
+};
+
+export const eject = async () => {
+  // I guess a way to remove service workers ain't too much to ask, eh?
+  // probs. a v4 thing
+  console.warn(yellow('Ejecting is still a Work-In-Progress.'));
+};
+
+export const packages = async (dir: string) => {
+  const pkgManager = await detectPackageManager(dir);
+
+  // TODO: Add `ora` as an extra flair
+  console.log(blue('Installing base packages for remix-pwa...\n\n'));
+  console.log(
+    yellow('installing:\n- @remix-pwa/sw\n- @remix-pwa/dev\n- @remix-pwa/worker-runtime\n- @remix-pwa/cache\n')
+  );
+  execSync(
+    `${pkgManager ?? 'npm'} ${
+      pkgManager === 'yarn' ? 'add' : 'install'
+    } @remix-pwa/sw @remix-pwa/dev @remix-pwa/worker-runtime @remix-pwa/cache`
+  );
 };
