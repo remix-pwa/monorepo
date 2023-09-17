e56b0d47839b712516e559120dac1d4e918f92bc
Abdur-Rahman <abbadshaf05@gmail.com>
2023-09-17 21:18:03 +0100

    chore(dev): updated CLI templates

--- packages/dev/cli/templates/app/precache.worker.js
+++ packages/dev/cli/templates/app/precache.worker.js
@@ -1,13 +1,73 @@
 /// <reference lib="WebWorker" />
 
-// Precache Worker
+import { Storage } from '@remix-pwa/cache';
+import { cacheFirst, networkFirst } from '@remix-pwa/strategy';
+import { PrecacheHandler, logger, matchRequest } from '@remix-pwa/sw';
+
+let self;
+
+const PAGES = 'page-cache';
+const DATA = 'data-cache';
+const ASSETS = 'assets-cache';
+
+// Open the caches and wrap them in `RemixCache` instances.
+const dataCache = Storage.open(DATA, {
+  ttl: 60 * 60 * 24 * 7 * 1_000, // 7 days
+});
+const documentCache = Storage.open(PAGES);
+const assetCache = Storage.open(ASSETS);
 
 self.addEventListener('install', event => {
-  console.log('Service worker installed');
+  logger.log('Service worker installed');
   event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener('activate', event => {
-  console.log('Service worker activated');
+  logger.log('Service worker activated');
   event.waitUntil(self.clients.claim());
+});
+
+const dataHandler = networkFirst({
+  cache: dataCache,
+});
+
+const assetsHandler = cacheFirst({
+  cache: assetCache,
+  cacheQueryOptions: {
+    ignoreSearch: true,
+    ignoreVary: true,
+  },
+});
+
+// The default fetch event handler will be invoke if the
+// route is not matched by any of the worker action/loader.
+export const defaultFetchHandler = ({ context, request }) => {
+  const type = matchRequest(request);
+
+  if (type === 'asset') {
+    return assetsHandler(context.event.request);
+  }
+
+  if (type === 'loader') {
+    return dataHandler(context.event.request);
+  }
+
+  return context.fetchFromServer();
+};
+
+const handler = new PrecacheHandler({
+  dataCache,
+  documentCache,
+  assetCache,
+  state: {
+    // A list of routes to ignore when precaching.
+    // Make sure to edit this list to match your app's routes.
+    // Alternatively, use ['*'] to ignore all routes.
+    // Or delete this option to precache all routes.
+    ignoredRoutes: route => route.id.includes('dashboard'),
+  },
+});
+
+self.addEventListener('message', event => {
+  event.waitUntil(handler.handle(event));
 });
