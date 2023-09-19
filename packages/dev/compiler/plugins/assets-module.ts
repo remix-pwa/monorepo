5a4e4f9cb1d8eeb65d181dd264840ff63e6baeb4
Abdur-Rahman <abbadshaf05@gmail.com>
2023-09-19 21:41:47 +0100

    chore(dev): removed asset plugin linting

--- packages/dev/compiler/plugins/assets-module.ts
+++ packages/dev/compiler/plugins/assets-module.ts
@@ -1,6 +1,7 @@
 import type { ResolvedWorkerConfig } from 'compiler/utils/config.js';
 import type { OnLoadResult, OnResolveArgs, OnResolveResult, Plugin, PluginBuild } from 'esbuild';
 import * as pkg from 'glob';
+
 const { glob } = pkg;
 
 const FILTER_REGEX = /@remix-sas\/dev\?assets/;
