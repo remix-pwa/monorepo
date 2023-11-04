e647a7cd332607afe154c3f7c1b6de0637e1d276
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-11-04 18:58:28 -0500

    convert playground to esm

--- playground/remix.config.js
+++ playground/remix.config.js
@@ -1,7 +1,6 @@
 /** @type {import('@remix-pwa/dev').WorkerConfig} */
-module.exports = {
+export default {
   ignoredRouteFiles: ['**/.*'],
-  serverModuleFormat: 'cjs',
   tailwind: true,
   serverDependenciesToBundle: [
     '@remix-pwa/sw',
