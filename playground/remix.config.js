2777514149043b9adf3b369e2a8870cb4d76c3b2
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-11-04 19:40:43 -0500

    don't bundle remix-pwa deps

--- playground/remix.config.js
+++ playground/remix.config.js
@@ -2,12 +2,5 @@
 export default {
   ignoredRouteFiles: ['**/.*'],
   tailwind: true,
-  serverDependenciesToBundle: [
-    '@remix-pwa/sw',
-    '@remix-pwa/strategy',
-    '@remix-pwa/dev',
-    '@remix-pwa/sync',
-    '@remix-pwa/cache',
-    'idb',
-  ],
+  serverDependenciesToBundle: ['idb'],
 };
