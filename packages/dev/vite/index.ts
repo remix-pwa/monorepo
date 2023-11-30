94ec0af4c0dda6151541e92888baab3e1b4030ea
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2023-11-30 17:37:23 +0100

    chore: cleaned up plugin index and exporting series of plugins instead

--- packages/dev/vite/index.ts
+++ packages/dev/vite/index.ts
@@ -1,5 +1,6 @@
-import type { IncomingMessage, ServerResponse } from 'http';
-import type { Connect, Plugin } from 'vite';
+import type { Plugin } from 'vite';
+
+import { LoaderPlugin } from './loader.js';
 
 export type PWAOptions = {
   registerSW: 'script' | 'none' | 'auto';
@@ -13,31 +14,6 @@ export type PWAOptions = {
  * })
  */
 
-export function RemixPWA(pwaOptions: Partial<PWAOptions> = {}): Plugin {
-  return {
-    name: 'remix-pwa-vite-plugin',
-    enforce: 'pre',
-    configResolved(config) {
-      console.log('configResolved', config);
-    },
-    configureServer(server) {
-      server.middlewares.use(
-        (req: Connect.IncomingMessage, res: ServerResponse<IncomingMessage>, next: Connect.NextFunction) => {
-          if (req.url?.includes('/@')) {
-            return next();
-          }
-
-          console.log('req', req.url, req.method, req.headers);
-          next();
-        }
-      );
-    },
-    async buildStart(options) {
-      console.log('buildStart', options);
-
-      // build service worker
-
-      // inject required code into main app bundle
-    },
-  };
+export function RemixPWA(pwaOptions: Partial<PWAOptions> = {}): Plugin[] {
+  return <Plugin[]>[LoaderPlugin(pwaOptions)];
 }
