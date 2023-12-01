f6cadfedcbf4f017c137a214d3c0ab7bb0cc4f88
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2023-12-01 14:49:21 +0100

    chore(dev): formulated a basic context api creator

--- packages/dev/vite/resolver.ts
+++ packages/dev/vite/resolver.ts
@@ -1,7 +1,32 @@
 import type { ResolvedConfig } from 'vite';
+import { normalizePath } from 'vite';
 
-import type { PWAOptions } from './types.js';
+import type { PWAOptions, ResolvedPWAOptions } from './types.js';
 
-export function resolveOptions(options: Partial<PWAOptions>, viteConfig: ResolvedConfig) {
-  const { registerSW = 'auto', serviceWorker = 'service-worker.js' } = options;
+export async function resolveOptions(
+  options: Partial<PWAOptions>,
+  viteConfig: ResolvedConfig
+): Promise<ResolvedPWAOptions> {
+  const {
+    minify = options.minify || false,
+    publicDir = options.publicDir ||
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- viteConfig.publicDir is always defined
+      normalizePath(viteConfig.publicDir).split(normalizePath(viteConfig.root)).pop()!,
+    registerSW = options.registerSW || 'script',
+    scope = options.scope || viteConfig.base,
+    serviceWorkerSrc = (options.serviceWorkerSrc || 'entry.worker.ts').trim(),
+  } = options;
+
+  const includeAssets = [/\.(js|css|html|svg|png|jpg|jpeg|webp)$/];
+  const excludeAssets = [/\.map$/, /^manifest.*\.json$/, /^sw\.js$/];
+
+  return {
+    minify,
+    publicDir: publicDir.replace(/^\/|\/$/g, ''),
+    registerSW,
+    scope,
+    serviceWorkerSrc: serviceWorkerSrc.replace(/^\/|\/$/g, ''),
+    includeAssets,
+    excludeAssets,
+  };
 }
