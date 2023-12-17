2c8c7dadf2831fe4cde73a3306364b94b4e10ddb
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2023-12-17 15:41:56 +0100

    chore(dev): new types and build configs :+1:

--- packages/dev/vite/resolver.ts
+++ packages/dev/vite/resolver.ts
@@ -25,15 +25,17 @@ export async function resolveOptions(
   viteConfig: ResolvedConfig
 ): Promise<ResolvedPWAOptions> {
   const {
+    entryWorkerFile: serviceWorkerFile = (options.entryWorkerFile || 'entry.worker.ts').trim(),
     ignoredSWRouteFiles = options.ignoredSWRouteFiles || [],
     registerSW = options.registerSW ?? 'script',
     scope = options.scope || viteConfig.base,
-    serviceWorkerFile = (options.serviceWorkerFile || 'entry.worker.ts').trim(),
     workerBuildDirectory = options.workerBuildDirectory ||
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- viteConfig.publicDir is always defined
       normalizePath(viteConfig.publicDir).split(normalizePath(viteConfig.root)).pop()!,
+    workerEntryPoint = options.workerEntryPoint || '@remix-pwa/worker-runtime',
     workerMinify = options.workerMinify || false,
     workerName = options.workerName || 'entry.worker',
+    workerSourceMap = options.workerSourceMap || false,
   } = options;
 
   const defaults: Partial<RemixVitePluginOptions> = {
@@ -49,18 +51,21 @@ export async function resolveOptions(
 
   const rootDirectory = viteConfig.root ?? process.env.REMIX_ROOT ?? process.cwd();
 
-  const { appDirectory, routes } = await resolveConfig(remixConfig, { rootDirectory });
+  const { appDirectory, assetsBuildDirectory, routes } = await resolveConfig(remixConfig, { rootDirectory });
 
   const includeAssets = [/\.(js|css|html|svg|png|jpg|jpeg|webp)$/];
   const excludeAssets = [/\.map$/, /^manifest.*\.json$/, /^sw\.js$/];
 
   return {
     workerMinify,
+    workerEntryPoint,
+    workerSourceMap,
     workerBuildDirectory: `${viteConfig.root}/${removeTrailingSlashes(workerBuildDirectory)}`,
     registerSW,
+    assetsBuildDirectory,
     scope,
     routes,
-    serviceWorkerFile: removeTrailingSlashes(serviceWorkerFile),
+    entryWorkerFile: removeTrailingSlashes(serviceWorkerFile),
     serviceWorkerPath: `${appDirectory}/${removeTrailingSlashes(serviceWorkerFile)}`,
     includeAssets,
     excludeAssets,
@@ -68,5 +73,5 @@ export async function resolveOptions(
     ignoredSWRouteFiles,
     rootDirectory,
     workerName,
-  };
+  } as ResolvedPWAOptions;
 }
--- packages/dev/vite/types.ts
+++ packages/dev/vite/types.ts
@@ -5,12 +5,14 @@ import type { ResolvedConfig } from 'vite';
 
 export interface PWAOptions extends RemixVitePluginOptions {
   registerSW: 'script' | null;
-  serviceWorkerFile: string;
+  entryWorkerFile: string;
   workerBuildDirectory: string;
   scope: string;
   workerMinify: boolean;
   workerName: string;
   ignoredSWRouteFiles: string[];
+  workerSourceMap: boolean;
+  workerEntryPoint: string;
   // future: unknown;
   // injectManifest: boolean; // wether manifest should also be injected. Do we handle manifest??
 }
@@ -19,12 +21,14 @@ export interface ResolvedPWAOptions
   extends Pick<
     Required<PWAOptions>,
     | 'registerSW'
-    | 'serviceWorkerFile'
+    | 'entryWorkerFile'
     | 'workerBuildDirectory'
     | 'scope'
     | 'workerMinify'
     | 'workerName'
+    | 'workerSourceMap'
     | 'ignoredSWRouteFiles'
+    | 'workerEntryPoint'
   > {
   includeAssets: RegExp[];
   excludeAssets: RegExp[];
@@ -32,6 +36,7 @@ export interface ResolvedPWAOptions
   appDirectory: string;
   serviceWorkerPath: string;
   routes: RouteManifest;
+  assetsBuildDirectory: string;
 }
 
 export interface PWAPluginContext {
@@ -39,4 +44,11 @@ export interface PWAPluginContext {
   pwaOptions: Partial<PWAOptions>;
   options: ResolvedPWAOptions;
   isDev: boolean;
+}
+
+export interface ResolvedEsbuildConfig {
+  routes: RouteManifest;
+  entryWorkerFile: string;
+  appDirectory: string;
+  assetsBuildDirectory: string;
 }
