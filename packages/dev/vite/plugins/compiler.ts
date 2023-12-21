f3d212a0755de0452b2104aef14ad25ef8a6cb67
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2023-12-21 23:11:22 +0100

    chore(dev): cleaned out the main compiler plugin + works in dev now :fire:!

--- packages/dev/vite/plugins/compiler.ts
+++ packages/dev/vite/plugins/compiler.ts
@@ -1,5 +1,5 @@
 import type { Context } from '@remix-run/dev/dist/compiler/context.js';
-import { whiteBright } from 'colorette';
+import { red, whiteBright } from 'colorette';
 import type { BuildOptions, Plugin as EsbuildPlugin } from 'esbuild';
 import { context } from 'esbuild';
 import type { FSWatcher, Plugin } from 'vite';
@@ -10,18 +10,14 @@ import { emptyModulesPlugin } from '../esbuild/empty-plugin.js';
 import entryModulePlugin from '../esbuild/entry-plugin.js';
 import routesModulesPlugin from '../esbuild/routes-plugin.js';
 import sideEffectsPlugin from '../esbuild/side-effects.js';
+import { getWorkerHash } from '../hash.js';
 
 export function CompilerPlugin(ctx: PWAPluginContext): Plugin {
   let watcher: FSWatcher | null = null;
-  let viteCommand: 'serve' | 'build';
 
   return <Plugin>{
     name: 'vite-plugin-remix-pwa:compiler',
-    config(_config, configEnv) {
-      viteCommand = configEnv.command;
-    },
-    // development only
-    configureServer({ watcher: viteWatcher, ws }) {
+    async configureServer({ watcher: viteWatcher, ws }) {
       const esbuildOptions: BuildOptions = {
         absWorkingDir: ctx.options.rootDirectory,
         entryPoints: {
@@ -51,11 +47,14 @@ export function CompilerPlugin(ctx: PWAPluginContext): Plugin {
 
       watcher = viteWatcher;
 
+      const MODE = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+      const TIME_LABEL = '💿 Built Service Worker in';
+      const DEV_TIME_LABEL = '💿 Re-built Service Worker in';
       const esbuildPluginContext = { config: ctx.options } as unknown as Context;
 
-      console.log(viteCommand, process.env.NODE_ENV);
+      let hash = '';
 
-      console.time('💿 Built Service Worker in');
+      console.time(TIME_LABEL);
 
       context({
         ...esbuildOptions,
@@ -73,25 +72,78 @@ export function CompilerPlugin(ctx: PWAPluginContext): Plugin {
           sideEffectsPlugin(ctx.options),
         ],
       }).then(async context => {
-        if (process.env.NODE_ENV === 'production') {
-          console.log(whiteBright('🏗️ Building Service Worker in production mode...\n'));
+        if (MODE === 'production') {
+          console.log(whiteBright(`🏗️ Building Service Worker in ${MODE} mode...\n`));
 
           await context.rebuild();
-          console.log('🎉 Service Worker built successfully!');
-          console.timeEnd('💿 Built Service Worker in');
+
+          console.log('\n🎉 Service Worker built successfully!');
+          console.timeEnd(TIME_LABEL);
 
           return await context.dispose();
         }
 
-        console.log(whiteBright('🏗️ Building Service Worker in development mode...\n'));
-      });
+        console.log(whiteBright(`🏗️ Building Service Worker in ${MODE} mode...\n`));
 
-      // viteWatcher.on('add', path => {});
+        viteWatcher.add(ctx.options.serviceWorkerPath);
+        await context.rebuild();
+        hash = getWorkerHash(ctx.options);
+
+        console.log('\n');
+        console.timeEnd(TIME_LABEL);
+
+        viteWatcher.on('add', async path => {
+          if (path.includes('routes') || path.includes(ctx.options.entryWorkerFile)) {
+            console.time(DEV_TIME_LABEL);
+
+            // Rebuild only added routes as well as workers
+            await context.rebuild();
+            hash = getWorkerHash(ctx.options);
+
+            console.timeEnd(DEV_TIME_LABEL);
+            console.log('Service worker hash (MD5):', hash);
+          }
+        });
+
+        viteWatcher.on('change', async path => {
+          if (path.includes('routes') || path.includes(ctx.options.entryWorkerFile)) {
+            console.time(DEV_TIME_LABEL);
+
+            // Rebuild only changed routes as well as workers
+            await context.rebuild();
+            hash = getWorkerHash(ctx.options);
+
+            console.timeEnd(DEV_TIME_LABEL);
+            console.log('Service worker hash (MD5):', hash);
+          }
+        });
+
+        viteWatcher.on('unlink', async path => {
+          if (path.includes('routes') || path.includes(ctx.options.entryWorkerFile)) {
+            console.time(DEV_TIME_LABEL);
+
+            // Rebuild only removed routes as well as workers
+            await context.rebuild();
+            hash = getWorkerHash(ctx.options);
+
+            console.timeEnd(DEV_TIME_LABEL);
+            console.log('Service worker hash (MD5):', hash);
+          }
+        });
+
+        viteWatcher.on('error', async () => {
+          console.error(red('🚨 Error occurred whilst watching files!'));
+
+          await context.cancel();
+          await context.dispose();
+          await viteWatcher.close();
+
+          process.exit(1);
+        });
+      });
     },
     closeBundle() {
-      if (watcher) {
-        watcher.close();
-      }
+      // 👀 what am I cooking here?
     },
   };
 }
