121b153bd56b756eb0e8f400fd38bedd6214d47a
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2023-12-21 19:37:13 +0100

    chore(dev): new exports :+1:

--- packages/dev/vite/index.ts
+++ packages/dev/vite/index.ts
@@ -2,10 +2,8 @@ import type { Plugin } from 'vite';
 
 import { createContext } from './context.js';
 import { CompilerPlugin } from './plugins/compiler.js';
-import { EntryVModPlugin } from './plugins/entry.js';
 import { LoaderPlugin } from './plugins/loader.js';
 import { EntryPlugin } from './plugins/main.js';
-import { RoutesVModPlugin } from './plugins/routes.js';
 import type { PWAOptions } from './types.js';
 
 /**
@@ -19,11 +17,5 @@ import type { PWAOptions } from './types.js';
 export function RemixPWA(pwaOptions: Partial<PWAOptions> = {}): Plugin[] {
   const ctx = createContext(pwaOptions);
 
-  return <Plugin[]>[
-    EntryPlugin(ctx),
-    CompilerPlugin(ctx),
-    LoaderPlugin(ctx),
-    EntryVModPlugin(ctx),
-    RoutesVModPlugin(ctx),
-  ];
+  return <Plugin[]>[EntryPlugin(ctx), CompilerPlugin(ctx), LoaderPlugin(ctx)];
 }
