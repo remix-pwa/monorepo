5f546a0419cb67fade27317b8db5bee656f8e858
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2023-12-23 16:02:04 +0100

    chore(dev): hash comparing utility - for hot reload client-side

--- packages/dev/vite/hash.ts
+++ packages/dev/vite/hash.ts
@@ -1,6 +1,7 @@
 import { createHash } from 'crypto';
 import { readFileSync } from 'fs';
 import { join } from 'node:path';
+import type { WebSocketServer } from 'vite';
 
 import type { ResolvedPWAOptions } from './types.js';
 
@@ -11,4 +12,19 @@ export const getWorkerHash = ({ workerBuildDirectory, workerName }: ResolvedPWAO
   hash.update(readFileSync(workerPath, { encoding: 'utf-8' }));
 
   return hash.digest('hex');
+};
+
+export const compareHash = (ws: WebSocketServer, oldHash: string, newHash: string) => {
+  if (oldHash === newHash) {
+    return;
+  }
+
+  ws.send({
+    type: 'custom',
+    event: 'pwa:worker-reload',
+    data: {
+      oldHash,
+      newHash,
+    },
+  });
 };
