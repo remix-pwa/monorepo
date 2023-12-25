686494d05a4f6f5257559a6bb5a6fa3e57c9de16
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2023-12-25 16:18:56 +0100

    fix(dev): added support for route worker APIs

--- packages/dev/vite/index.ts
+++ packages/dev/vite/index.ts
@@ -4,18 +4,11 @@ import { createContext } from './context.js';
 import { CompilerPlugin } from './plugins/compiler.js';
 import { LoaderPlugin } from './plugins/loader.js';
 import { EntryPlugin } from './plugins/main.js';
+import { StripRoutesPlugin } from './plugins/strip-routes.js';
 import type { PWAOptions } from './types.js';
-
-/**
- * The way this would work is:
- *
- * RemixPwa({
- *  registerSW: 'script' | null,
- * })
- */
 
 export function RemixPWA(pwaOptions: Partial<PWAOptions> = {}): Plugin[] {
   const ctx = createContext(pwaOptions);
 
-  return <Plugin[]>[EntryPlugin(ctx), CompilerPlugin(ctx), LoaderPlugin(ctx)];
+  return <Plugin[]>[EntryPlugin(ctx), StripRoutesPlugin(ctx), CompilerPlugin(ctx), LoaderPlugin(ctx)];
 }
