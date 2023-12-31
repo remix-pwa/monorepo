ae86ac4171a33e19481afa2047608b1472dccd78
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2023-12-31 22:41:56 +0100

    chore(sw): added a sub-path index export for `cache` submodule

---
+++ packages/sw/src/cache/index.ts
@@ -0,0 +1,15 @@
+export { BaseStrategy } from './BaseStrategy.js';
+export { CacheFirst } from './CacheFirst.js';
+export { CacheOnly } from './CacheOnly.js';
+export { EnhancedCache } from './EnhancedCache.js';
+export { NetworkFirst } from './NetworkFirst.js';
+export { NetworkOnly } from './NetworkOnly.js';
+export { StaleWhileRevalidate } from './StaleWhileRevalidate.js';
+export type {
+  CacheFriendlyOptions,
+  CacheOptions,
+  CacheStats,
+  EnhancedCacheOptions,
+  NetworkFriendlyOptions,
+  StrategyName,
+} from './types.js';
