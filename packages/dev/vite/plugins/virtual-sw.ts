89b9cfb361369d1a4ccc4bfc0de35913d5f2baf9
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-03-15 01:29:57 +0100

    chore(dev): new singular plugin for virtual modules

--- packages/dev/vite/plugins/virtual-sw.ts
+++ packages/dev/vite/plugins/virtual-sw.ts
@@ -1,44 +1,18 @@
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
-import { normalizePath, type Plugin } from 'vite';
+import type { Plugin } from 'vite';
 import type { PWAPluginContext } from 'vite/types.js';
 
 import * as VirtualModule from '../vmod.js';
 
 export function VirtualSWPlugins(ctx: PWAPluginContext): Plugin[] {
-  const vmodId = VirtualModule.id('sw');
+  const vmodId = VirtualModule.id('entry-sw');
 
   return <Plugin[]>[
     {
-      name: 'vite-plugin-remix-pwa:virtual-sw',
-      configureServer(server) {
-        server.watcher.add(normalizePath(resolve(ctx.options.appDirectory, ctx.options.entryWorkerFile)));
-      },
-      handleHotUpdate({ file, server }) {
-        if (file === normalizePath(resolve(ctx.options.appDirectory, ctx.options.entryWorkerFile))) {
-          const swModule = server.moduleGraph.getModuleById(vmodId);
-
-          if (swModule) {
-            server.moduleGraph.invalidateModule(swModule);
-            return [];
-          }
-        }
-      },
-      resolveId(id) {
-        if (id === vmodId) {
-          return { id };
-        }
-      },
+      name: 'vite-plugin-remix-pwa:virtual-entry-sw',
       load(id) {
         if (id === vmodId) {
-          const swPath = resolve(ctx.options.appDirectory, ctx.options.entryWorkerFile);
-          const swCode = readFileSync(swPath, 'utf-8');
-          return swCode;
+          return 'export const msg = "hello world from virtual";';
         }
-      },
-      buildEnd() {
-        // Additional build steps if needed
-        console.log('Built virtual sw module!');
       },
     },
   ];
