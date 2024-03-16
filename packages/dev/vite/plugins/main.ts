4310a4b997bb8dda7ba17ae35d4b42a9575f4d42
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-03-16 01:00:53 +0100

    chore(dev): entry plugin now uses correct plugin context

--- packages/dev/vite/plugins/main.ts
+++ packages/dev/vite/plugins/main.ts
@@ -1,16 +1,20 @@
 import type { Plugin } from 'vite';
-import type { PWAPluginContext } from 'vite/types.js';
 
 import { resolveOptions } from '../resolver.js';
+import type { PWAOptions, PWAPluginContext } from '../types.js';
 
-export function EntryPlugin(ctx: PWAPluginContext): Plugin {
+export function EntryPlugin(ctx: PWAPluginContext, pwaOptions: Partial<PWAOptions>): Plugin {
   return <Plugin>{
     name: 'vite-plugin-remix-pwa:entry',
     enforce: 'pre',
     async configResolved(config) {
       ctx.isDev = config.mode === 'development';
+      // @ts-ignore - Utilizing remix special config here
+      ctx.isRemixDevServer = config.__remixPluginContext !== undefined;
       ctx.viteConfig = config;
-      ctx.options = await resolveOptions(ctx.pwaOptions, config);
+      // @ts-ignore - Also utilizing remix special config here
+      ctx.__remixPluginContext = config.__remixPluginContext ?? undefined;
+      ctx.options = await resolveOptions(pwaOptions, config);
     },
   };
 }
