2db0a9d77b1b98bd1baee9aec47afc785f236b2a
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-03-17 22:11:08 +0100

    chore(dev): prepping for v4

--- packages/dev/index.ts
+++ packages/dev/index.ts
@@ -1,13 +1,81 @@
-export { run } from './cli/run.js';
-
-// For debugging processes I guess, you would almost never use this
-// and you shouldn't
-export { runCompiler } from './compiler/compiler.js';
-export type { WorkerConfig as AppConfig, WorkerConfig } from './compiler/utils/config.js';
-
-// todo: Automatically add "WebWorker" to the user tsconfig
-
 // Vite ✨
-// eslint-disable-next-line camelcase
-export { RemixPWA as unstable_RemixPWA } from './vite/index.js';
+export { RemixPWA as remixPWA } from './vite/index.js';
 export type { PWAOptions as PWAViteOptions } from './vite/types.js';
+
+export interface WebAppManifest {
+  name?: string;
+  short_name?: string;
+  description?: string;
+  icons?: Array<{
+    src: string;
+    sizes?: string;
+    type?: string;
+    purpose?: 'any' | 'maskable' | 'monochrome';
+  }>;
+  start_url?: string;
+  display?: 'fullscreen' | 'standalone' | 'minimal-ui' | 'browser';
+  display_override?: Array<'window-controls-overlay' | 'bordered' | 'standard'>;
+  orientation?:
+    | 'any'
+    | 'natural'
+    | 'landscape'
+    | 'landscape-primary'
+    | 'landscape-secondary'
+    | 'portrait'
+    | 'portrait-primary'
+    | 'portrait-secondary';
+  dir?: 'ltr' | 'rtl' | 'auto';
+  lang?: string;
+  prefer_related_applications?: boolean;
+  related_applications?: Array<{
+    platform: string;
+    url?: string;
+    id?: string;
+    min_version?: string;
+    fingerprints?: Array<{
+      type: string;
+      value: string;
+    }>;
+  }>;
+  scope?: string;
+  screenshots?: Array<{
+    src: string;
+    sizes?: string;
+    type?: string;
+    platform?: string;
+    label?: string;
+  }>;
+  shortcuts?: Array<{
+    name?: string;
+    short_name?: string;
+    description?: string;
+    url?: string;
+    icons?: Array<{
+      src: string;
+      sizes?: string;
+      type?: string;
+      purpose?: 'any' | 'maskable' | 'monochrome';
+    }>;
+  }>;
+  share_target?: {
+    action?: string;
+    method?: 'GET' | 'POST';
+    enctype?: string;
+    params?: {
+      [key: string]: {
+        name?: string;
+        title?: string;
+        description?: string;
+      };
+    };
+  };
+  protocol_handlers?: Array<{
+    protocol: string;
+    url: string;
+  }>;
+  note?: string;
+  background_color?: string;
+  theme_color?: string;
+  categories?: Array<string>;
+  iarc_rating_ids?: Array<string>;
+}
