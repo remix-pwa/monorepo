44409b7a44504db42bda58be8467df925fb109c0
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-03-18 18:42:42 +0100

    test(cli): updated tests to match current criteria

--- packages/cli/__test__/cli.test.ts
+++ packages/cli/__test__/cli.test.ts
@@ -1,15 +1,19 @@
+import { resolve } from 'pathe';
 import { describe, expect, test, vi } from 'vitest';
 
 import program from '../cli';
 
 describe('CLI test suite', () => {
-  test('CLI should generate default manifest when prompted', () => {
-    const consoleSpy = vi.spyOn(console, 'log');
+  test('CLI should manifest command should throw an error when file path not found', () => {
+    const consoleLogSpy = vi.spyOn(console, 'log');
+    const consoleErrorSpy = vi.spyOn(console, 'error');
 
     program.parse(['node', 'cli.js', 'manifest']);
 
-    expect(consoleSpy).toHaveBeenCalledWith('Generating web manifest file...', { ts: true, js: undefined });
+    expect(consoleLogSpy).toHaveBeenCalledWith('Generating TypeScript web manifest file...');
+    expect(consoleErrorSpy).toHaveBeenCalledWith(`The directory "${resolve(process.cwd())}/app/routes" does not exist`);
 
-    consoleSpy.mockRestore();
+    consoleLogSpy.mockRestore();
+    consoleErrorSpy.mockRestore();
   });
 });
