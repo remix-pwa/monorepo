bb563754e1b00273f03b4df7b921131a23bebe78
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-03-18 16:21:57 +0100

    chore(cli): still running up some tests

--- packages/cli/cli.ts
+++ packages/cli/cli.ts
@@ -1,11 +1,25 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --no-warnings
 
 import { Command } from '@commander-js/extra-typings';
+import { existsSync } from 'fs';
+import { readFile, writeFile } from 'fs/promises';
+import { resolve } from 'pathe';
 import colors from 'picocolors';
+import type { CompilerOptions } from 'typescript';
+import ts from 'typescript';
 
 const packageJson = await import('./package.json', { assert: { type: 'json' } });
 
 const { magenta } = colors;
+const { ModuleKind, ScriptTarget, transpileModule } = ts;
+const __dirname = import.meta.dirname;
+
+const compilerOptions: CompilerOptions = {
+  module: ModuleKind.ES2022,
+  target: ScriptTarget.ES2022,
+  allowJs: true,
+  lib: ['ES2022', 'WebWorker'],
+};
 
 const program = new Command();
 
@@ -31,8 +45,13 @@ program
   .description('Generate a web manifest file')
   .option('--js', 'Generate JavaScript web manifest file')
   .option('--ts', 'Generate TypeScript web manifest file (default)')
-  .action(options => {
-    let { js, ts } = options;
+  .option(
+    '-d, --dest <outFile>',
+    'Destination directory for the service worker file',
+    'app/routes/manifest[.webmanifest].ts'
+  )
+  .action(async options => {
+    let { dest, js, ts } = options;
 
     if (js && ts) {
       console.error('You can only generate one type of web manifest file');
@@ -43,9 +62,48 @@ program
       ts = true;
     }
 
-    // Generate web manifest file here...
+    if (js) {
+      // Generate JavaScript web manifest file here...
+      console.log('Generating JavaScript web manifest file...');
 
-    console.log('Generating web manifest file...', { js, ts });
+      const path = resolve(process.cwd(), dest);
+      const dirPath = path.split('/').slice(0, -1).join('/');
+
+      if (!existsSync(dirPath)) {
+        console.error(`The directory "${dirPath}" does not exist`);
+        process.exit(1);
+      }
+
+      const fileName = path.split('/').pop() ?? 'manifest[.webmanifest].js';
+
+      const templateContent = await readFile(resolve(__dirname, 'templates', 'manifest[.webmanifest].ts'), 'utf-8');
+      const { diagnostics, outputText } = transpileModule(templateContent, {
+        compilerOptions,
+      });
+
+      if (diagnostics && diagnostics.length) {
+        console.error(diagnostics);
+        process.exit(1);
+      }
+
+      await writeFile(resolve(process.cwd(), dirPath, fileName.replace(/\.ts$/, '.js')), outputText);
+    } else {
+      // Generate TypeScript web manifest file here...
+      console.log('Generating TypeScript web manifest file...');
+
+      const path = resolve(process.cwd(), dest);
+      const dirPath = path.split('/').slice(0, -1).join('/');
+
+      if (!existsSync(dirPath)) {
+        console.error(`The directory "${dirPath}" does not exist`);
+        process.exit(1);
+      }
+
+      const fileName = path.split('/').pop() ?? 'manifest[.webmanifest].ts';
+      const templateContent = await readFile(resolve(__dirname, 'templates', 'manifest[.webmanifest].ts'), 'utf-8');
+
+      await writeFile(resolve(process.cwd(), dirPath, fileName.replace(/\.js$/, '.ts')), templateContent);
+    }
   });
 
 program
@@ -53,26 +111,63 @@ program
   .alias('service-worker')
   .description('Generate a service worker file')
   .option('--js', 'Generate JavaScript service worker file')
-  .option('-d, --dest <outFile>', 'Destination directory for the service worker file', 'app/routes/entry.worker.ts')
+  .option('-d, --dest <outFile>', 'Destination directory for the service worker file', 'app/entry.worker.ts')
   // .option('-t, --type <sw-type>', 'Type of service worker to generate', 'workbox') // todo: later
-  .action(options => {
+  .action(async options => {
     const { dest, js } = options;
 
     if (js) {
       // Generate JavaScript service worker file here...
-      console.log('Generating JavaScript service worker file...', { dest });
+      console.log('Generating JavaScript service worker file...');
+
+      const path = resolve(process.cwd(), dest);
+      const dirPath = path.split('/').slice(0, -1).join('/');
+
+      if (!existsSync(dirPath)) {
+        console.error(`The directory "${dirPath}" does not exist`);
+        process.exit(1);
+      }
+
+      const fileName = path.split('/').pop() ?? 'entry.worker.js';
+
+      const templateContent = await readFile(resolve(__dirname, 'templates', 'entry.worker.ts'), 'utf-8');
+      const { diagnostics, outputText } = transpileModule(templateContent, {
+        compilerOptions,
+      });
+
+      if (diagnostics && diagnostics.length) {
+        console.error(diagnostics);
+        process.exit(1);
+      }
+
+      await writeFile(resolve(process.cwd(), dirPath, fileName.replace(/\.ts$/, '.js')), outputText);
     } else {
       // Generate TypeScript service worker file here...
-      console.log('Generating TypeScript service worker file...', { dest });
+      console.log('Generating TypeScript service worker file...');
+
+      const path = resolve(process.cwd(), dest);
+      const dirPath = path.split('/').slice(0, -1).join('/');
+
+      if (!existsSync(dirPath)) {
+        console.error(`The directory "${dirPath}" does not exist`);
+        process.exit(1);
+      }
+
+      const fileName = path.split('/').pop() ?? 'entry.worker.ts';
+      const templateContent = await readFile(resolve(__dirname, 'templates', 'entry.worker.ts'), 'utf-8');
+
+      await writeFile(resolve(process.cwd(), dirPath, fileName.replace(/\.js$/, '.ts')), templateContent);
     }
   });
 
-program
-  .command('icons')
-  .description('Generate favicons and app icons')
-  .action(() => {
-    // Generate favicons and app icons here...
-    console.log('Generating favicons and app icons...');
-  });
+// program
+//   .command('icons')
+//   .description('Generate favicons and app icons')
+//   .action(() => {
+//     // Generate favicons and app icons here...
+//     console.log('Generating favicons and app icons...');
+//   });
 
 program.parse(process.argv);
+
+// export default program;
