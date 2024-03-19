453378a2cd40428dd69d8fc3a9a9bb1f7b5fd832
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-03-19 22:23:11 +0100

    test(sw): created tests for `SWR`

---
+++ packages/sw/src/cache/__test__/stale-while-revalidate.test.ts
@@ -0,0 +1,156 @@
+import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, test, vi } from 'vitest';
+import createFetchMock from 'vitest-fetch-mock';
+
+import { StaleWhileRevalidate } from '../StaleWhileRevalidate.js';
+import CacheMock from './utils/browser-cache-mock.js';
+
+const fetchMocker = createFetchMock(vi);
+
+describe('StaleWhileRevalidate Strategy Testing Suite', () => {
+  let mockCache: Cache;
+
+  beforeAll(() => {
+    fetchMocker.enableMocks();
+  });
+
+  beforeEach(() => {
+    fetchMocker.doMock();
+
+    mockCache = new CacheMock();
+    global.caches = {
+      open: vi.fn().mockResolvedValue(mockCache),
+      delete: vi.fn().mockResolvedValue(true),
+      has: vi.fn().mockResolvedValue(true),
+      keys: vi.fn().mockResolvedValue([]),
+      match: vi.fn().mockResolvedValue(new Response()),
+    };
+  });
+
+  test('should return a cached response if available', async () => {
+    const strategy = new StaleWhileRevalidate('test-cache');
+    const mockResponse = new Response('cached response', {
+      headers: { 'sw-cache-timestamp': Date.now().toString() },
+    });
+    const cache = await caches.open('test-cache');
+
+    const spiedOnCache = vi.spyOn(cache, 'match');
+
+    await cache.put('http://localhost/test', mockResponse);
+
+    const response = await strategy.handleRequest('http://localhost/test');
+    const text = await response.text();
+
+    expect(text).toBe('cached response');
+
+    expect(global.caches.open).toHaveBeenCalledWith('test-cache');
+    expect(spiedOnCache).toHaveBeenCalledOnce();
+
+    spiedOnCache.mockRestore();
+  });
+
+  test('should fetch from network if not in cache', async () => {
+    const strategy = new StaleWhileRevalidate('test-cache');
+    fetchMocker.mockResponseOnce('network response');
+
+    const response = await strategy.handleRequest('http://localhost/not-in-cache');
+    const text = await response.text();
+
+    expect(text).toBe('network response');
+    expect(fetchMocker.requests()[0].url).toEqual('http://localhost/not-in-cache');
+  });
+
+  test('should fallback to cache if network request fails', async () => {
+    const strategy = new StaleWhileRevalidate('test-cache');
+    fetchMocker.mockRejectOnce(new Error('Network Error'));
+
+    const mockResponse = new Response('cached response', {
+      headers: { 'sw-cache-timestamp': Date.now().toString() },
+    });
+    const cache = await caches.open('test-cache');
+
+    await cache.put('http://localhost/test', mockResponse);
+
+    const response = await strategy.handleRequest('http://localhost/test');
+    const text = await response.text();
+
+    expect(text).toBe('cached response');
+  });
+
+  test('should not update cache if network request fails', async () => {
+    const strategy = new StaleWhileRevalidate('test-cache');
+    fetchMocker.mockRejectOnce(new Error('Network Error'));
+
+    const mockResponse = new Response('cached response', {
+      headers: { 'sw-cache-timestamp': Date.now().toString() },
+    });
+    const cache = await caches.open('test-cache');
+    await cache.put('http://localhost/test', mockResponse);
+
+    const spiedOnCache = vi.spyOn(cache, 'put');
+
+    await strategy.handleRequest('http://localhost/test');
+
+    expect(spiedOnCache).not.toHaveBeenCalled();
+
+    spiedOnCache.mockRestore();
+  });
+
+  test('should update cache with network response if valid', async () => {
+    const strategy = new StaleWhileRevalidate('test-cache');
+    fetchMocker.mockResponseOnce('network response');
+
+    const cache = await caches.open('test-cache');
+    const spiedOnCache = vi.spyOn(cache, 'put');
+
+    await strategy.handleRequest('http://localhost/to-cache');
+
+    expect(spiedOnCache).toHaveBeenCalledOnce();
+
+    spiedOnCache.mockRestore();
+  });
+
+  test.skip("should not update cache if the response status isn't valid", async () => {
+    const strategy = new StaleWhileRevalidate('test-cache');
+    fetchMocker.mockResponseOnce('network response', { status: 404 });
+
+    const cache = await caches.open('test-cache');
+    const spiedOnCache = vi.spyOn(cache, 'put');
+
+    await strategy.handleRequest('http://localhost/to-cache');
+
+    expect(spiedOnCache).not.toHaveBeenCalled();
+
+    spiedOnCache.mockRestore();
+  });
+
+  test('should handle concurrent requests to the same resource efficiently', async () => {
+    const strategy = new StaleWhileRevalidate('test-cache');
+    const cache = await caches.open('test-cache');
+    const spiedOncache = vi.spyOn(cache, 'put');
+
+    fetchMocker.mockResponseOnce('network response');
+
+    const requestPromises = Array.from({ length: 5 }, () => strategy.handleRequest('http://localhost/concurrent'));
+
+    const responses = await Promise.all(requestPromises);
+
+    responses.forEach(response => {
+      expect(response).toBeInstanceOf(Response);
+      expect(response.status).toBe(200);
+      expect(response.ok).toBeTruthy();
+    });
+
+    expect(fetchMocker.requests().length).toBe(1);
+    expect(spiedOncache).toHaveBeenCalledOnce();
+  });
+
+  afterEach(() => {
+    fetchMocker.resetMocks();
+    vi.restoreAllMocks();
+  });
+
+  afterAll(() => {
+    fetchMocker.disableMocks();
+    vi.clearAllMocks();
+  });
+});
