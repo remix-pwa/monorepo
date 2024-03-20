f03ccf647a53d8621b27dd0fb49e971f76bdcff4
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-03-20 18:11:17 +0100

    chore(playground): upgraded `remix-pwa`

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,9 +1,9 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { unstable_RemixPWA as remixPwa } from "@remix-pwa/dev";
 import { defineConfig } from "vite";
 import { installGlobals } from "@remix-run/node";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { PWAOptions } from "@remix-pwa/dev/vite/types";
+import { PWAViteOptions } from "@remix-pwa/dev";
+import { remixPWA } from '@remix-pwa/dev';
 
 installGlobals();
 
@@ -13,7 +13,7 @@ export default defineConfig({
       ignoredRouteFiles: ["**/.*"],
     }),
     tsconfigPaths(),
-    remixPwa(<Partial<PWAOptions>>{
+    remixPWA(<Partial<PWAViteOptions>>{
       // workerMinify: true,
       // workerSourceMap: true,
     }),
