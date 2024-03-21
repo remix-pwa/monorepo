78d5a6f96002f4ea1de9bfd72d22ff0a43456c0e
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-03-21 21:45:03 +0100

    chore(sw): extended `Logger` - it now allows for dynamic style override + log level override

--- packages/sw/src/logger/logger.ts
+++ packages/sw/src/logger/logger.ts
@@ -1,9 +1,18 @@
 export type LogLevel = 'debug' | 'info' | 'log' | 'warn' | 'error' | 'groupCollapsed' | 'groupEnd';
 
+interface LoggerStyles {
+  background: string;
+  borderRadius: string;
+  color: string;
+  fontWeight: string;
+  padding: string;
+  [key: string]: string;
+}
+
 interface LoggerOptions {
   prefix: string;
   styles: {
-    [level in LogLevel]: string;
+    [level in LogLevel]: LoggerStyles;
   };
   logLevel: LogLevel;
   isProductionEnv: boolean;
@@ -13,13 +22,58 @@ export class Logger {
   static defaultOptions: LoggerOptions = {
     prefix: 'remix-pwa',
     styles: {
-      debug: 'background: #7f8c8d; color: white; border-radius: 0.5em; font-weight: bold; padding: 2px 0.5em;',
-      info: 'background: #3498db; color: white; border-radius: 0.5em; font-weight: bold; padding: 2px 0.5em;',
-      log: 'background: #2ecc71; color: white; border-radius: 0.5em; font-weight: bold; padding: 2px 0.5em;',
-      warn: 'background: #f39c12; color: white; border-radius: 0.5em; font-weight: bold; padding: 2px 0.5em;',
-      error: 'background: #c0392b; color: white; border-radius: 0.5em; font-weight: bold; padding: 2px 0.5em;',
-      groupCollapsed: 'background: #3498db; color: white; border-radius: 0.5em; font-weight: bold; padding: 2px 0.5em;',
-      groupEnd: 'color: white; border-radius: 0.5em; font-weight: bold; padding: 2px 0.5em;',
+      debug: {
+        background: '#7f8c8d',
+        color: 'white',
+        borderRadius: '0.5em',
+        fontWeight: 'bold',
+        padding: '2px 0.5em',
+      },
+      info: {
+        background: '#3498db',
+        color: 'white',
+        borderRadius: '0.5em',
+        fontWeight: 'bold',
+        padding: '2px 0.5em',
+      },
+      log: {
+        background: '#2ecc71',
+        color: 'white',
+        borderRadius: '0.5em',
+        fontWeight: 'bold',
+        padding: '2px 0.5em',
+      },
+      warn: {
+        background: '#f39c12',
+        color: 'white',
+        borderRadius: '0.5em',
+        fontWeight: 'bold',
+        padding: '2px 0.5em',
+      },
+      error: {
+        background: '#c0392b',
+        color: 'white',
+        borderRadius: '0.5em',
+        fontWeight: 'bold',
+        padding: '2px 0.5em',
+      },
+      groupCollapsed: {
+        background: '#3498db',
+        color: 'white',
+        borderRadius: '0.5em',
+        fontWeight: 'bold',
+        padding: '2px 0.5em',
+      },
+      groupEnd: {
+        // @ts-expect-error - no bg color on `groupEnd`
+        background: null,
+        color: 'white',
+        borderRadius: '0.5em',
+        fontWeight: 'bold',
+        padding: '2px 0.5em',
+      },
+      // groupCollapsed: 'background: #3498db; color: white; border-radius: 0.5em; font-weight: bold; padding: 2px 0.5em;',
+      // groupEnd: 'color: white; border-radius: 0.5em; font-weight: bold; padding: 2px 0.5em;',
     },
     logLevel: 'debug',
     isProductionEnv: process.env.NODE_ENV === 'production',
@@ -30,6 +84,14 @@ export class Logger {
 
   constructor(options?: Partial<LoggerOptions>) {
     this.options = { ...Logger.defaultOptions, ...options };
+  }
+
+  setLogLevel(level: LogLevel) {
+    this.options.logLevel = level;
+  }
+
+  setStyles(styles: Partial<LoggerOptions['styles']>) {
+    this.options.styles = { ...this.options.styles, ...styles };
   }
 
   private print(level: LogLevel, args: any[]) {
@@ -47,7 +109,10 @@ export class Logger {
       }
     }
 
-    const logPrefix = this.inGroup ? [] : [`%c${this.options.prefix}`, styles[level]];
+    const styleProps = Object.entries(styles[level])
+      .map(([prop, value]) => `${prop}: ${value}`)
+      .join('; ');
+    const logPrefix = this.inGroup ? [] : [`%c${this.options.prefix}`, styleProps];
     console[method](...logPrefix, ...args);
 
     if (method === 'groupCollapsed') {
