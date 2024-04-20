684c00f75e76fb2ff57eb0568cecdab7a0883136
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-04-20 22:30:44 +0100

    chore: added `push` package to sandbox

--- packages/push/server/storage.ts
+++ packages/push/server/storage.ts
@@ -1,5 +1,9 @@
 import type { PushSubscription } from './types.js';
 
+/**
+ * A general subscription storage provider you can implement. This can be used as a guideline to store
+ * subscriptions in a database, in memory, or anywhere else you'd like.
+ */
 export interface SubscriptionStorageProvider {
   storeSubscription(subscription: PushSubscription, id: string): Promise<void>;
   deleteSubscription(subscription: PushSubscription, id: string): Promise<void>;
