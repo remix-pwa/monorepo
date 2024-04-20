7ffa3d2739d444e3cf3baab3ba63cc89a905e7e9
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-04-20 23:03:13 +0100

    chore: testing out the push package

--- playground/src/app/entry.worker.ts
+++ playground/src/app/entry.worker.ts
@@ -1,6 +1,7 @@
 /// <reference lib="WebWorker" />
 
 import { EnhancedCache, isDocumentRequest, isLoaderRequest, logger, NavigationHandler } from '@remix-pwa/sw';
+import { PushManager } from '@remix-pwa/push';
 
 declare let self: ServiceWorkerGlobalScope;
 
@@ -84,4 +85,8 @@ const msgHandler = new NavigationHandler({
 
 self.addEventListener('message', async event => {
   await msgHandler.handleMessage(event);
+})
+
+new PushManager({
+  
 })
