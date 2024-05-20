f0530fc7f8b871057a684f2dddbd40f7c986a2ed
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-05-20 11:36:56 +0100

    fix(sw): re-fixing opaque response patch

--- packages/sw/src/cache/NetworkFirst.ts
+++ packages/sw/src/cache/NetworkFirst.ts
@@ -62,12 +62,12 @@ export class NetworkFirst extends BaseStrategy {
   }
 
   private async validateResponse(response: Response): Promise<boolean> {
-    if (!this.cacheableResponse) {
-      return true;
-    }
-
     if (this.isOpaqueResponse(response)) {
       return false;
+    }
+
+    if (!this.cacheableResponse) {
+      return true;
     }
 
     const { headers = {}, statuses = [] } = this.cacheableResponse;
