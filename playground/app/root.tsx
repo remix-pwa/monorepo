c5a7b7e6a509f26d62b42417a1fd59b27745443e
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-07-21 14:51:03 +0100

    background styles

--- playground/app/root.tsx
+++ playground/app/root.tsx
@@ -70,7 +70,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <ClientHints />
         <Links />
       </head>
-      <body className="size-full">
+      <body className="size-full scroll-smooth background">
         {children}
         <ScrollRestoration />
         <Scripts />
