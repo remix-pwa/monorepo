e003c05b437d131ccdb08eb172cc64764734455f
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-07-23 19:20:35 +0100

    chore: placeholder theme switching

--- playground/app/routes/_showcase+/__header.tsx
+++ playground/app/routes/_showcase+/__header.tsx
@@ -1,16 +1,18 @@
 import { Disclosure } from '@headlessui/react'
+import { ThemeSwitcher } from '~/components/ThemeSwitcher'
 import { cn } from '~/utils'
 
 export const Header = () => {
   return (
     <Disclosure as="header" className={cn(
-      'fixed top-0 z-40 w-full flex-none backdrop-blur bg-transparent border-0 px-4 py-3 md:pl-6',
+      'fixed top-0 z-40 w-full flex-none backdrop-blur bg-transparent border-0 px-4 py-3 md:pl-6 flex justify-between',
       ''
     )}>
       <h3>Showcase</h3>
       {/* Theme Switcher, etc. */}
       {/* Disclosure Button */}
       {/* Disclosure Panel - Mobile Menu */}
+      <ThemeSwitcher />
     </Disclosure>
   )
 }
