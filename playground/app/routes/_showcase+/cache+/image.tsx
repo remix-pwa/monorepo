36484595fa5ce076dc47d4bb75dd8476d2c4b5e7
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-08-06 16:21:43 +0100

    chore: extending the site to multiple pages

--- playground/app/routes/_showcase+/cache+/image.tsx
+++ playground/app/routes/_showcase+/cache+/image.tsx
@@ -1,7 +1,23 @@
+import { Page, PageContent, PageFooter, PageTitle } from "~/components";
+import { TableOfContents } from "~/types";
+
+export const handle = {
+  tableOfContents: []
+} as { tableOfContents: TableOfContents };
+
 export default function Component() {
   return (
-    <div>
-      image
-    </div>
+    <Page>
+      <PageTitle>
+        Caching Images
+      </PageTitle>
+      <PageContent>
+        {''}
+      </PageContent>
+      <PageFooter
+        prev={{ title: 'Caching Text Content', url: '/cache/text' }}
+        next={{ title: 'Media stuffs', url: '/cache/media' }}
+      />
+    </Page>
   )
 }
